2c9d7edd1f0e0a84b92d7f5813cb700bc64dab70
TengNg <nguyentin148@gmail.com>
2023-05-25 09:15:27 +0700

    feat: update styling

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -5,7 +5,7 @@ export default function Title({ titleName }) {
         <>
             <div className='flex flex-col justify-center items-center select-none m-[0_0_2rem_0]'>
                 <h1
-                    className="relative text-center font-bold underline--style underline--hover transition all hover:text-gray-500"
+                    className="relative text-center font-bold underline--style underline--hover transition all hover:text-gray-500 mt-1"
                 >{titleName}</h1>
             </div>
         </>
